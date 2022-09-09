9e14ebec325de10b566f21b88a1d83c6f97a9af5
ereinha <74273926+ereinha@users.noreply.github.com>
2022-09-09 12:06:59 -0500

    Add files via upload

--- create_data.py
+++ create_data.py
@@ -1,9 +1,11 @@
 import numpy as np
 from sklearn.model_selection import train_test_split
 
+# Create equal amounts of signal and background data with 3 parameters
 signal = np.random.normal(loc=1, scale=.4, size=(100000,3))
 background = np.random.normal(loc=0, scale=.2, size=(100000,3))
 
+# Split the data into 75% training data and 25% validation data
 signal_train, \
 signal_valid, \
 background_train, \
@@ -12,6 +14,7 @@ background_valid = train_test_split(signal,
                                   test_size=.25, 
                                   random_state=12)
 
+# Save the data
 np.save('./signal_train.npy', signal_train)
 np.save('./signal_valid.npy', signal_valid)
 np.save('./bg_train.npy', background_train)
